90408978474067428c520600b89969c9aea14ebb
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-12-19 13:12:45 -0500

    Fix Express 5 wildcard route syntax
    Change catch-all route from '*' to '/*' for Express 5 compatibility

--- server/src/server.js
+++ server/src/server.js
@@ -67,7 +67,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
 
   // Catch-all route: serve index.html for any non-API routes (React Router)
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
   });
 }
